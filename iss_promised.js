a6de3864d3c95bdb0735ec97414be5464f41e69b
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-07 03:31:15 +0000

    Updated iss file with promises

---
+++ iss_promised.js
@@ -0,0 +1,44 @@
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+/* 
+ * Makes a request to ipvigilante.com using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  //return request(`https://ipvigilante.com/json/${ip}`);
+  return request(`http://ip-api.com/json/${ip}`)
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body).data;
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
+  //return request(url);
+};
+
+/* 
+ * Input: None
+ * Returns: Promise for fly over data for users location
+ */
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
+
