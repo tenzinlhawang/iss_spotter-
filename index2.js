67cfbf433d29a6a45d671771ad8d9f1a33aeb7ee
tenzinlhawang <tenzin.10zin@hotmaill.com>
2020-11-07 03:30:19 +0000

    Updated index file with promises

---
+++ index2.js
@@ -0,0 +1,24 @@
+// index2.js
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
